caef83f8fe48a1da247e4640fe4a4c19a7aaf096
ct <2016201028@qq.com>
2022-08-08 11:59:45 +0800

    git005

--- src/main/java/com/ctgit/text/text01.java
+++ src/main/java/com/ctgit/text/text01.java
@@ -5,5 +5,7 @@ public class text01 {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git4");
     }
 }
